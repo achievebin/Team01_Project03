56f283cd2c4d26f11f3424047b89848f8be1457b
achievebin <achieve7384@gmail.com>
2023-05-16 15:56:01 +0900

    Add SQL File

---
+++ Team01_Project03/src/reserve/ResSQL.sql
@@ -0,0 +1,20 @@
+--예약 테이블
+create table reservationtbl(
+    res_number number(5)primary key,
+    act_number number(5) ,
+    res_start date,
+    res_end date,
+    res_name varchar2(30),
+    res_phone varchar2(30),
+    res_purchase varchar2(30),
+    res_price varchar2(30),
+    res_hotel varchar2(30));   
+    
+--예약번호 시퀀스
+create sequence seq_res_num 
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
